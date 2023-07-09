7e20318589cc7bcac83aac6f37fde5f383a455fc
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2023-07-08 21:22:00 -0400

    add module dashboard

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard.component';
-import { HomeComponent } from './components/home/home.component';
-import { RouterModule } from '@angular/router';
-
-
+import { HomeComponent } from './components/home/home.component'
+import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     HomeComponent
   ],
+
   imports: [
     CommonModule,
-    RouterModule
+    SharedModule,
+    MaterialModule
+
   ]
 })
 export class DashboardModule { }
