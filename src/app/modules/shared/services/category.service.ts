081634644897d6fd228c41b46dc3aa3dc3de18bf
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2023-07-08 22:49:05 -0400

    add a method in CategoryService get all categories

--- src/app/modules/shared/services/category.service.ts
+++ src/app/modules/shared/services/category.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
+//call environment, declare constant
+const base_url = environment.base_url;
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+    /**
+     * get all categories
+     * @returns get all categories existing
+     */
+  getCategories(){
+      // endpoint 
+      const endpoint =`${base_url}/categories`;
+      return this.http.get(endpoint);
+    }
 }
