eaf0a05071f5263e89e91643a5fecf0f4cb8229f
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2023-07-08 22:16:23 -0400

    add imports classes

--- src/app/modules/category/category.module.ts
+++ src/app/modules/category/category.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './components/category.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../shared/material.module';
 
 
 
@@ -9,7 +12,11 @@ import { CategoryComponent } from './components/category.component';
     CategoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class CategoryModule { }
