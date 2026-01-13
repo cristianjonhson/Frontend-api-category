355c6218cec951fde414583faed517ba6be221f2
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-13 11:33:31 -0300

    feat: mejorar unit test del Home

--- Frontend/src/app/modules/dashboard/components/home/home.component.spec.ts
+++ Frontend/src/app/modules/dashboard/components/home/home.component.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomeComponent } from './home.component';
+import { CategoryService } from 'src/app/modules/shared/services/category.service'; 
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  const categoryServiceMock = {
+    getCategories: () => of([])
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+      imports: [RouterTestingModule],
+      providers: [{ provide: CategoryService, useValue: categoryServiceMock }]
+    }).compileComponents();
   });
 
   beforeEach(() => {
