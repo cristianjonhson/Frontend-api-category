a146d3c188a6b338a9be78a0bb1e1ba99459ffdd
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-24 19:55:54 -0300

    feat: creacion del logica vista componente productos

---
+++ Frontend/src/app/modules/product/components/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+  products: any[] = [];
+  displayedColumns: string[] = ['name', 'price'];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
+}
