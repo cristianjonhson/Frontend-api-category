ef617be03d093112a469c11fdfed74fca32bb4f9
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-27 22:31:47 -0300

    feat: agregar logica de negocio para agregar productos

--- Frontend/src/app/modules/product/services/product.service.ts
+++ Frontend/src/app/modules/product/services/product.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
+  private baseUrl = 'api';
+
+  constructor(private http: HttpClient) {}
+
   private products = [
     { name: 'Producto 1', price: 100, category: 'Electrónica', quantity: 10 },
     { name: 'Producto 2', price: 200, category: 'Hogar', quantity: 5 },
@@ -14,4 +19,9 @@ export class ProductService {
   getProducts(): Observable<any[]> {
     return of(this.products);
   }
+
+  createProduct(payload: any) {
+  return this.http.post<any>(`${this.baseUrl}/products`, payload);
+}
+
 }
