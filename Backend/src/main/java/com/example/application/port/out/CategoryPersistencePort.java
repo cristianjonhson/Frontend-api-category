220d4705cbe87d47dd1124039defc9383d5ba13f
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-28 13:08:52 -0300

    feat: se agrego la busqueda por id agregada al puerto de categorias

--- Backend/src/main/java/com/example/application/port/out/CategoryPersistencePort.java
+++ Backend/src/main/java/com/example/application/port/out/CategoryPersistencePort.java
@@ -3,8 +3,10 @@ package com.example.application.port.out;
 import com.example.domain.model.Category;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryPersistencePort {
     List<Category> findAll();
+    Optional<Category> findById(Long id);
     Category save(Category category);
 }
