9750576f8a2b435d0a9c3590a930397772c1b237
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-28 12:35:20 -0300

    feat: Se amplio ApiResponse para incluir productResponse y se agrego la estructura ProductResponse

--- Backend/src/main/java/com/example/application/dto/ApiResponse.java
+++ Backend/src/main/java/com/example/application/dto/ApiResponse.java
@@ -6,6 +6,7 @@ public class ApiResponse<T> {
 
     private List<Metadata> metadata;
     private CategoryResponse<T> categoryResponse;
+    private ProductResponse<T> productResponse;
 
     public ApiResponse() {}
 
@@ -14,11 +15,19 @@ public class ApiResponse<T> {
         this.categoryResponse = categoryResponse;
     }
 
+    public ApiResponse(List<Metadata> metadata, ProductResponse<T> productResponse) {
+        this.metadata = metadata;
+        this.productResponse = productResponse;
+    }
+
     public List<Metadata> getMetadata() { return metadata; }
     public void setMetadata(List<Metadata> metadata) { this.metadata = metadata; }
 
     public CategoryResponse<T> getCategoryResponse() { return categoryResponse; }
     public void setCategoryResponse(CategoryResponse<T> categoryResponse) { this.categoryResponse = categoryResponse; }
+
+    public ProductResponse<T> getProductResponse() { return productResponse; }
+    public void setProductResponse(ProductResponse<T> productResponse) { this.productResponse = productResponse; }
 
     public static class Metadata {
         private String code;
@@ -38,5 +47,15 @@ public class ApiResponse<T> {
 
         public List<T> getCategory() { return category; }
         public void setCategory(List<T> category) { this.category = category; }
+    }
+
+    public static class ProductResponse<T> {
+        private List<T> product;
+
+        public ProductResponse() {}
+        public ProductResponse(List<T> product) { this.product = product; }
+
+        public List<T> getProduct() { return product; }
+        public void setProduct(List<T> product) { this.product = product; }
     }
 }
