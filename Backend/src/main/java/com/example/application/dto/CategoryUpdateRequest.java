e25cf0e8d6bffa57ef9d8990aeccd0e30fdb8b8a
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-29 16:07:29 -0300

    feat: Se agrega Dto de Categoria para actualizar

---
+++ Backend/src/main/java/com/example/application/dto/CategoryUpdateRequest.java
@@ -0,0 +1,25 @@
+package com.example.application.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class CategoryUpdateRequest {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    public CategoryUpdateRequest() {}
+
+    public CategoryUpdateRequest(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+}
