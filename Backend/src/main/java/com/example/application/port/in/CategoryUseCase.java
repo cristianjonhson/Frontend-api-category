225d66b9bc830ea7b06fe1b77bf26da4012f20ae
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-29 15:53:34 -0300

    add: incorporacion en el puerto de Categoria para eliminar y actualizar

--- Backend/src/main/java/com/example/application/port/in/CategoryUseCase.java
+++ Backend/src/main/java/com/example/application/port/in/CategoryUseCase.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface CategoryUseCase {
     List<Category> getAll();
     Category create(Category category);
+    Category update(Long id, Category category);
+    void delete(Long id);
 }
