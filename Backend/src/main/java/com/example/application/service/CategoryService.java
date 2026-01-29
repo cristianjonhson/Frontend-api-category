916ab2ed47140e6649f1b78710531743c2676d25
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-29 16:02:54 -0300

    add: incorporacion en el service de Categoria para eliminar y actualizar

--- Backend/src/main/java/com/example/application/service/CategoryService.java
+++ Backend/src/main/java/com/example/application/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.example.application.service;
 import com.example.application.port.in.CategoryUseCase;
 import com.example.application.port.out.CategoryPersistencePort;
 import com.example.domain.model.Category;
+import com.example.infrastructure.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,5 +27,23 @@ public class CategoryService implements CategoryUseCase {
         // ✅ id debe ser null al crear
         category.setId(null);
         return persistence.save(category);
+    }
+
+    @Override
+    public Category update(Long id, Category category) {
+        Category existing = persistence.findById(id)
+                .orElseThrow(() -> new NotFoundException("Category not found"));
+
+        existing.setName(category.getName());
+        existing.setDescription(category.getDescription());
+
+        return persistence.save(existing);
+    }
+
+    @Override
+    public void delete(Long id) {
+        persistence.findById(id)
+                .orElseThrow(() -> new NotFoundException("Category not found"));
+        persistence.delete(id);
     }
 }
