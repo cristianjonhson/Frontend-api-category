2218dba9f501e0a047c712aa0247d59a279a2283
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-30 11:41:40 -0300

    add: incorporacion en la persistencia (puerto) de Producto para eliminar

--- Backend/src/main/java/com/example/application/port/in/ProductUseCase.java
+++ Backend/src/main/java/com/example/application/port/in/ProductUseCase.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface ProductUseCase {
     List<Product> getAll();
     Product create(Product product, Long categoryId);
+    Product update(Long id, Product product, Long categoryId);
+    void delete(Long id);
 }
--- Backend/src/main/java/com/example/application/port/out/ProductPersistencePort.java
+++ Backend/src/main/java/com/example/application/port/out/ProductPersistencePort.java
@@ -3,8 +3,11 @@ package com.example.application.port.out;
 import com.example.domain.model.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductPersistencePort {
     List<Product> findAll();
+    Optional<Product> findById(Long id);
     Product save(Product product);
+    void delete(Long id);
 }
