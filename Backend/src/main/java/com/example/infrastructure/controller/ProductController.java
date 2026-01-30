c446851cadd47a5abceec8b399fa6b4d54cac3a9
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-30 12:04:23 -0300

    add: incorporacion en el controller de Producto para eliminar y actualizar

--- Backend/src/main/java/com/example/infrastructure/controller/ProductController.java
+++ Backend/src/main/java/com/example/infrastructure/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.infrastructure.controller;
 import com.example.application.dto.ApiResponse;
 import com.example.application.dto.ProductCreateRequest;
 import com.example.application.dto.ProductDTO;
+import com.example.application.dto.ProductUpdateRequest;
 import com.example.application.mapper.ProductMapper;
 import com.example.application.port.in.ProductUseCase;
 import com.example.domain.model.Product;
@@ -50,5 +51,28 @@ public class ProductController {
         );
 
         return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<ProductDTO>> updateProduct(
+            @PathVariable Long id,
+            @Valid @RequestBody ProductUpdateRequest request) {
+        Product updated = productUseCase.update(id,
+                ProductMapper.toDomainForCreate(request.getName(), request.getPrice(), request.getQuantity()),
+                request.getCategoryId()
+        );
+
+        ApiResponse<ProductDTO> response = new ApiResponse<>(
+                List.of(new ApiResponse.Metadata("SUCCESS")),
+                new ApiResponse.ProductResponse<>(List.of(ProductMapper.toDTO(updated)))
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productUseCase.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
