8a35c01f35ea20eced15785bddd8bac267cca459
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-02-03 23:43:43 -0300

    feat: se creo Barrel export para importaciones simplificadas (constantes)

---
+++ Frontend/src/app/shared/constants/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Barrel export de todas las constantes compartidas
+ * Facilita las importaciones en otros módulos
+ */
+
+// Constantes de API
+export * from './api.constants';
+
+// Constantes de interfaz de usuario
+export * from './ui.constants';
+
+// Constantes de mensajes
+export * from './messages.constants';
