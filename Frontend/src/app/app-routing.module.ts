906c94a2510fa121448aa3fbaeaa75e8f7dc209e
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-02-13 10:33:33 -0300

    feat: imports absolutos a rutas relativas en el Frontend en el modulo de ruteo

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ROUTES, ROUTE_PATHS } from 'src/app/shared/constants/routes.constants';
+import { ROUTES, ROUTE_PATHS } from './shared/constants/routes.constants';
 
 /**
  * Configuración de rutas principales de la aplicación
