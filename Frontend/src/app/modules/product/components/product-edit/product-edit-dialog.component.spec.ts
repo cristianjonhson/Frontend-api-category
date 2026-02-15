adbd505db7ac58bbc75f9114ccf8498c8b2d8ba6
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-02-15 12:41:20 -0300

    feat: crear prueba unitaria para componente editar product

---
+++ Frontend/src/app/modules/product/components/product-edit/product-edit-dialog.component.spec.ts
@@ -0,0 +1,56 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
+
+import { ProductEditDialogComponent } from './product-edit-dialog.component';
+import { ProductService } from '../../services/product.service';
+import { SweetAlertService } from '../../../../shared/services';
+
+describe('ProductEditDialogComponent', () => {
+  let component: ProductEditDialogComponent;
+  let fixture: ComponentFixture<ProductEditDialogComponent>;
+
+  const productServiceMock = {
+    updateProduct: jasmine.createSpy('updateProduct').and.returnValue(of(null))
+  };
+
+  const sweetAlertMock = {
+    showError: jasmine.createSpy('showError')
+  };
+
+  const dialogRefMock = {
+    close: jasmine.createSpy('close')
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ProductEditDialogComponent],
+      providers: [
+        FormBuilder,
+        { provide: ProductService, useValue: productServiceMock },
+        { provide: SweetAlertService, useValue: sweetAlertMock },
+        { provide: MatDialogRef, useValue: dialogRefMock },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            product: { id: 1, name: 'Prod', price: 10, quantity: 1, categoryId: 1, categoryName: 'Cat' },
+            categories: [{ id: 1, name: 'Cat', description: 'Desc' }]
+          }
+        }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductEditDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
