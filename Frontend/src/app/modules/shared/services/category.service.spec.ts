6e123affcdeb756c31a54f00413eb4e978fe2fc4
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-02-16 13:02:33 -0300

    feat: crear prueba unitaria para servicio de categoria

--- Frontend/src/app/modules/shared/services/category.service.spec.ts
+++ Frontend/src/app/modules/shared/services/category.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CategoryService } from './category.service';
+import { LoggerService } from '../../../core/services/logger.service';
 
 describe('CategoryService', () => {
   let service: CategoryService;
+  const loggerMock = {
+    info: jasmine.createSpy('info'),
+    warn: jasmine.createSpy('warn'),
+    error: jasmine.createSpy('error')
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: LoggerService, useValue: loggerMock }]
+    });
     service = TestBed.inject(CategoryService);
   });
 
