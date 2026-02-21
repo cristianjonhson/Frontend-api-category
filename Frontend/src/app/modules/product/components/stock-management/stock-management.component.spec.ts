afba03269f54f407cc8fc96ea130cc4b97f5cc14
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-02-20 21:29:40 -0300

    feat: Agregué pruebas unitarias específicas de onPageChange en componente Gestion-Stock

--- Frontend/src/app/modules/product/components/stock-management/stock-management.component.spec.ts
+++ Frontend/src/app/modules/product/components/stock-management/stock-management.component.spec.ts
@@ -4,6 +4,7 @@ import { of } from 'rxjs';
 
 import { StockManagementComponent } from './stock-management.component';
 import { ProductService } from '../../services/product.service';
+import { PaginatorService } from '../../../../shared/services';
 
 describe('StockManagementComponent', () => {
   let component: StockManagementComponent;
@@ -13,10 +14,21 @@ describe('StockManagementComponent', () => {
     getProducts: jasmine.createSpy('getProducts').and.returnValue(of([]))
   };
 
+  const paginatorServiceMock = {
+    createDataSource: jasmine.createSpy('createDataSource').and.callFake(() => ({ data: [] })),
+    connect: jasmine.createSpy('connect'),
+    setData: jasmine.createSpy('setData'),
+    resetToFirstPage: jasmine.createSpy('resetToFirstPage'),
+    handlePageChange: jasmine.createSpy('handlePageChange')
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [StockManagementComponent],
-      providers: [{ provide: ProductService, useValue: productServiceMock }],
+      providers: [
+        { provide: ProductService, useValue: productServiceMock },
+        { provide: PaginatorService, useValue: paginatorServiceMock }
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
@@ -29,5 +41,25 @@ describe('StockManagementComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should delegate onPageChange to paginator service', () => {
+    const pageEvent = {
+      pageIndex: 1,
+      pageSize: 20,
+      length: 120,
+      previousPageIndex: 0
+    };
+    const paginatorRef = {} as any;
+
+    (component as any).sharedPaginator = { paginator: paginatorRef } as any;
+
+    component.onPageChange(pageEvent as any);
+
+    expect(paginatorServiceMock.handlePageChange).toHaveBeenCalledWith(
+      pageEvent as any,
+      component.dataSource,
+      paginatorRef
+    );
   });
 });
