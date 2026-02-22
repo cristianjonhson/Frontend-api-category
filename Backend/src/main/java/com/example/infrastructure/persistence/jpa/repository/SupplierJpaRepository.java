18de693bdc16a5f64501e07da647950a40a44f69
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-02-21 22:11:30 -0300

    feat: se crea capa Persistencia JPA Proveedor (Repository)

---
+++ Backend/src/main/java/com/example/infrastructure/persistence/jpa/repository/SupplierJpaRepository.java
@@ -0,0 +1,9 @@
+package com.example.infrastructure.persistence.jpa.repository;
+
+import com.example.infrastructure.persistence.jpa.entity.SupplierJpaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierJpaRepository extends JpaRepository<SupplierJpaEntity, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, Long id);
+}
