ba7d59c0df4e4dbf62cc9a31320853046c843a5f
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-02-23 11:49:13 -0300

    fix: Se extiende relacion Proveedor-Producto en capa JPA Producto (Repository)

--- Backend/src/main/java/com/example/infrastructure/persistence/jpa/repository/ProductJpaRepository.java
+++ Backend/src/main/java/com/example/infrastructure/persistence/jpa/repository/ProductJpaRepository.java
@@ -3,5 +3,8 @@ package com.example.infrastructure.persistence.jpa.repository;
 import com.example.infrastructure.persistence.jpa.entity.ProductJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductJpaRepository extends JpaRepository<ProductJpaEntity, Long> {
+	List<ProductJpaEntity> findBySupplierId(Long supplierId);
 }
