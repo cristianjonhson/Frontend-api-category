1f41372fb28e3fae877bce268cd136a7b5314561
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-02-26 13:02:45 -0300

    feat: modulo actualizado, se declara nuevo componente de creacion de proveedory se importa el ReactiveForms

--- Frontend/src/app/modules/supplier/supplier.module.ts
+++ Frontend/src/app/modules/supplier/supplier.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { SupplierListComponent } from './components/supplier-list/supplier-list.component';
+import { SupplierCreateDialogComponent } from './components/supplier-add/supplier-create-dialog.component';
 
 @NgModule({
-  declarations: [SupplierListComponent],
+  declarations: [SupplierListComponent, SupplierCreateDialogComponent],
   imports: [
     CommonModule,
     SharedModule,
+    ReactiveFormsModule,
     MatTableModule,
     MatPaginatorModule
   ],
