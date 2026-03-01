670356aa5587ab74ed69d555741e40e3d0c085b3
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-03-01 19:27:54 -0300

    feat: se añade constante de filtrado de productos

--- Frontend/src/app/shared/constants/app.constants.ts
+++ Frontend/src/app/shared/constants/app.constants.ts
@@ -3,6 +3,7 @@
  */
 export const APP_CONFIG = {
   STORAGE_KEYS: {
-    HOME_SHOW_TIPS: 'home_show_tips'
+    HOME_SHOW_TIPS: 'home_show_tips',
+    PRODUCT_LIST_FILTERS: 'product_list_filters'
   }
 } as const;
