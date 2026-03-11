03505185d8ba9789e6178b05d6c3f497d8600b1b
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-03-11 13:38:14 -0300

    add: se crea barrels de interface por feature (Productos)

---
+++ Frontend/src/app/modules/product/components/interfaces/index.ts
@@ -0,0 +1,4 @@
+export * from '../product-list/product-list-filters.interface';
+export * from '../stock-management/stock-row.interface';
+export * from '../product-add/product-create-dialog-data.interface';
+export * from '../product-edit/product-edit-dialog-data.interface';
