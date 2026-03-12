92f5c3fb78a3be6f524017d3209470bffa4d4abd
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-03-12 13:51:43 -0300

    add: se crea barrels de componentes por feature (Proveedores)

---
+++ Frontend/src/app/modules/supplier/components/index.ts
@@ -0,0 +1,4 @@
+export * from './supplier-list/supplier-list.component';
+export * from './supplier-add/supplier-create-dialog.component';
+export * from './supplier-edit/supplier-edit-dialog.component';
+export * from './interfaces';
