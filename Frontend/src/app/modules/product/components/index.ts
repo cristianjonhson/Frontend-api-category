b826b33b14cbece997443dce1a2ec5cf4db77081
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-03-12 13:51:29 -0300

    add: se crea barrels de componentes por feature (Productos)

---
+++ Frontend/src/app/modules/product/components/index.ts
@@ -0,0 +1,5 @@
+export * from './product-list/product-list.component';
+export * from './product-add/product-create-dialog.component';
+export * from './product-edit/product-edit-dialog.component';
+export * from './stock-management/stock-management.component';
+export * from './interfaces';
