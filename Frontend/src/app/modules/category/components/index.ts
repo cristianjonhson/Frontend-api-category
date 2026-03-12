50869b40343c604c3bc6d94439156aad5ba6e86f
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-03-12 13:52:00 -0300

    add: se crea barrels de componentes por feature (categorias)

---
+++ Frontend/src/app/modules/category/components/index.ts
@@ -0,0 +1,4 @@
+export * from './list-category/category.component';
+export * from './add-category/add-category.component';
+export * from './edit-category/edit-category.component';
+export * from './interfaces';
