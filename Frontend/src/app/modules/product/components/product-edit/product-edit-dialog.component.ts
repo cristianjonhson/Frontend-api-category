c6c08838e4ef11958d07b498119d0aa9777c4c8c
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-03-14 21:09:50 -0300

    fix: Actualicé imports del componente Editar Productos en el nuevo feature

--- Frontend/src/app/modules/product/components/product-edit/product-edit-dialog.component.ts
+++ Frontend/src/app/modules/product/components/product-edit/product-edit-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { finalize } from 'rxjs/operators';
-import { ProductService } from '../../services/product.service';
+import { ProductService } from '../../services';
 import { ERROR_MESSAGES, VALIDATION_RULES } from '../../../../shared/constants';
 import { ICategory, IProduct, IProductRequest, ISupplier } from '../../../../shared/interfaces';
 import { SweetAlertService } from '../../../../shared/services';
